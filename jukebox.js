8b5c66b60c31cf1bef84085a2ecfdb0da5d0fa98
Jkb6873 <jakub.taraska96@gmail.com>
2018-06-01 12:12:38 -0400

    fixed event listeners

--- jukebox.js
+++ jukebox.js
@@ -1,28 +1,21 @@
 class JukeBox{
-  this.playlist = [{},{},{}];
-  this.play = document.getElementById("play").addEventListener("click", play());
-  this.pause = document.getElementById("pause").addEventListener("click", pause());
-  this.skip = document.getElementById("skip").addEventListener("click", skip());
-  this.loop = document.getElementById("loop").addEventListener("click", loop());
-  this.select = document.getElementById("select").addEventListener("click", select());
 
-  //Play()
-  function play(){
-          music.play();
-  }
-  //Pause()
-  function pause(){
-      music.pause()
-  }
-  //Skip()
-  function skip(){
-  }
-  //Stop()
-  function stop(){
-
-
-  }
-  //Loop()
-  function loop(){
-  }
 }
+
+
+document.getElementById("play").addEventListener("click", function(){
+  console.log("this is play");
+});
+
+document.getElementById("pause").addEventListener("click",  function(){
+  console.log("this is pause");
+});
+document.getElementById("skip").addEventListener("click",  function(){
+  console.log("this is skip");
+});
+document.getElementById("loop").addEventListener("click",  function(){
+  console.log("this is loop");
+});
+document.getElementById("select").addEventListener("click",  function(){
+  console.log("this is select");
+});
