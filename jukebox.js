d81f2c5b33e219b7468dbb5198afaf791653f42d
Zach Yuzon <ZachYuzon@HON-2016391.local>
2018-06-01 11:08:59 -0400

    update jukebox.js

---
+++ jukebox.js
@@ -0,0 +1,17 @@
+
+//ID for audio element
+var music = document.getElementById("music");
+//ID for play button
+var pButton = document.getElementById("play")
+function playAudio(){
+        music.play();
+}
+
+function pauseAudio(){
+    music.pause()
+}
+//Play()
+//Pause()
+//Skip()
+//Stop()
+//Loop()
